3b6610a0a2a448696c26a02e3f7419fec9b5dd2a
Christian Jensen <Oragada@gmail.com>
2013-09-14 12:48:19 +0200

    Initial setup of the Component system

---
+++ PlatformerEngine/PlatformerEngine/Components/Platform.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PlatformerEngine.Components
+{
+    abstract class Platform : Collidable
+    {
+    }
+}
